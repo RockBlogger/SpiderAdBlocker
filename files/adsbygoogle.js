960641b09b0737f9ce4a7cdbae0e33d6357f66ca
Spider Blogging <88818251+RockBlogger@users.noreply.github.com>
2023-03-03 16:47:02 +0530

    Create adsbygoogle.js

---
+++ files/adsbygoogle.js
@@ -0,0 +1,14 @@
+/**
+
+    Anti AdBlock - AdBlock Killer
+    Version 2.0
+    Developed by RockBlogger (Telegram: @RockBlogger)
+    For more information and to purchase, visit https://spiderblogging.in
+    
+    
+    This is a Dummy file.
+
+
+
+*/
+  
