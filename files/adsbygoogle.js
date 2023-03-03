2794e334d08087b5ba392dbaa25533baed1dc8a7
Spider Blogging <88818251+RockBlogger@users.noreply.github.com>
2023-03-03 16:50:40 +0530

    Update adsbygoogle.js

--- files/adsbygoogle.js
+++ files/adsbygoogle.js
@@ -1,11 +1,1 @@
-/**
 
-    Version 2.0
-    Developed by RockBlogger (Telegram: @RockBlogger)
-    For more information and to purchase, visit https://spiderblogging.in
-    
-    
-    This is a Dummy file.
-
-*/
-  
